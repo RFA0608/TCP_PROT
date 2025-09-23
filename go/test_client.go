8f52af6f3b9773b743c2ebcdff1f4287121dd5b3
RFA0608 <159212138+RFA0608@users.noreply.github.com>
2025-09-23 15:14:54 +0900

    Add files via upload

--- go/test_client.go
+++ go/test_client.go
@@ -9,7 +9,7 @@ func main() {
 	conn := InitTCP(host, port)
 
 	for i := 0; i < 10; i++ {
-		_, data := recv(conn)
+		_, data := Recv(conn)
 		fmt.Println(data)
 		Send(conn, "go 인덱스:"+fmt.Sprintf("%d", i))
 	}
--- python/test_server.py
+++ python/test_server.py
@@ -5,9 +5,10 @@ PORT = 9999
 
 with tcs.tcp_server(HOST, PORT) as tcsp:
     tcsp.set_metric(True, 1000)
-    tcsp.set_samepleconst(False, 10)
+    tcsp.set_sampleconst(False, 10)
     for i in range(10):
-        tcsp.send("python 인덱스:"+str(i))
+        # tcsp.send("python 인덱스:"+str(i))
+        tcsp.send(i)
         k, l = tcsp.recv()
         print(l)
 
